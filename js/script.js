f68c953fdf0c6fa9c06bf00420a4fdb278279a7c
Maurizio-P <mauriziopignotti@gmail.com>
2021-10-26 17:20:34 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,1 +1,164 @@
 "use strict"
+
+const Listicons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+/*
+    1 - ciclo sul mio array per recuperare ogni singola icona
+    2 - pusho in un nuovo array l'icona presa ad ogni ciclo in base al tipo
+    3 - stampo in html il mio array, in base a cosa seleziono nel select
+
+*/
+
+const select = document.getElementById("category-icons")
+const row = document.querySelector(".row")
+const valueSelect = document.getElementById("category-icons").value
+console.log(valueSelect);
+
+
+const arrayAnimal = []
+const arrayVegetable = []
+const arrayUser = []
+
+function forArray(array){
+    for (let i = 0; i < array.length; i++) {
+        const {type} = array[i];
+        if (type === "animal"){
+            arrayAnimal.push(array[i])
+        }
+        if (type === "vegetable"){
+            arrayVegetable.push(array[i])
+        }
+        if (type === "user"){
+            arrayUser.push(array[i])
+        }
+    }
+}
+
+
+function stampInHTML(array){
+    row.innerHTML = ""
+    for (let i = 0; i < array.length; i++) {
+        const {name, prefix, type, family} = array[i];
+        
+        row.innerHTML += `  <div class="my-card ${type}">
+                                <i class="${family} ${prefix}${name}"></i>
+                                <h5>${name}</h5>
+                            </div>`
+    }
+}
+
+
+forArray(Listicons)
+select.addEventListener("change", function () {
+    
+    if (select.value === "all") {
+        stampInHTML(Listicons)
+    }
+    if (select.value === "animal") {
+        stampInHTML(arrayAnimal)
+    }
+    if (select.value === "vegetable") {
+        stampInHTML(arrayVegetable)
+    }
+    if (select.value === "user") {
+        stampInHTML(arrayUser)
+    }
+    
+})
